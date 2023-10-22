17f96e97432e68f641c673c72b4c237bc27e5682
AlexandrPoddubnyi <San4oss91@gmail.com>
2023-10-22 20:09:11 +0300

    fixed problem with delete contact

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -42,7 +42,7 @@ const contactsSlice = createSlice({
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
-        const index = state.items.findIndex(item => item === action.payload.id)
+        const index = state.items.findIndex(item => item.id === action.payload.id)
         state.items.splice(index, 1)
       })
       .addCase(deleteContact.rejected, (state, action) => {
